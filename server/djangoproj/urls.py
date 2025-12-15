ac47f2d01e0f010be60b7cfe950f72a722a55f03
danyalmudassar <danyalmudassar823@gmail.com>
2025-12-15 08:41:59 -0500

    Completed static pages: Added Contact.html content, styles, and updated urls.py.

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -13,6 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+f"""djangoproj URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView  # This is correctly imported
@@ -26,4 +41,7 @@ urlpatterns = [
     # --- START: New path for About Us page ---
     path('about/', TemplateView.as_view(template_name="About.html")),
     # --- END: New path for About Us page ---
+    # --- START: New path for Contact Us page (Added to resolve 404) ---
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    # --- END: New path for Contact Us page ---
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
